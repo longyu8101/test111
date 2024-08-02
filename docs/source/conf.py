b75fd421343382673fa5dfe7c57e04737dc4980f
xiaoyao <2233865125@qq.com>
2024-08-02 13:42:12 +0800

    update115

---
+++ docs/source/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '结构设计原理<br /><br />第5版'
+copyright = '哈尔滨工业大学' 
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+language ='zh'
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+html_style = 'css/custom.css'
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "sphinxawesome_theme"
+
+# html_theme = 'sphinx_book_theme'
+
+html_title = "结构设计原理  第5版"
+# html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
+
+
+extensions = ['recommonmark']
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst','.md']
