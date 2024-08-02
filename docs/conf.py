380262c0c5011c5b1ce86c5e3f7855f5fbc3db45
xiaoyao <2233865125@qq.com>
2024-08-02 13:59:56 +0800

    update125

--- docs/conf.py
+++ docs/conf.py
@@ -29,12 +29,12 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sphinx_rtd_theme
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme = "sphinxawesome_theme"
 
-# html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_book_theme'
 
 html_title = "结构设计原理  第5版"
 # html_logo = "_static/logo.png"
